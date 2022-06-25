56320eaf02e0d70022e3e37a465bbad16a74bead
Dmitriy Voytenko <mailvoytenko@gmail.com>
2022-06-25 14:18:34 +0300

    Added skeleton of the project.

---
+++ src/main/java/com/github/gitvoytenko/springinaction/tococloud/TocoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.github.gitvoytenko.springinaction.tococloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gitvoytenko/springinaction/tococloud/TocoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gitvoytenko.springinaction.tococloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
